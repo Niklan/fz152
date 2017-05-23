6e9ccb1569fe1436c5763f34e4f84ddf07810e43
armrus <armrus@1957854.no-reply.drupal.org>
2017-05-23 21:03:35 +0500

    Issue #2880657 by armrus: Privacy policy text not showed

--- src/Controller/Fz152PrivacyPolicyPage.php
+++ src/Controller/Fz152PrivacyPolicyPage.php
@@ -18,13 +18,13 @@ class Fz152PrivacyPolicyPage extends ControllerBase {
    * {@inheritdoc}
    */
   public function content() {
-    $config = \Drupal::config('fz152.settings');
+    $config = \Drupal::config('fz152.privacy_policy_page');
     $output = [];
     // Set page title
-    $output['#title'] = $config->get('privacy_policy_page.title');
+    $output['#title'] = $config->get('title');
 
     // Set page content
-    $text = $config->get('privacy_policy_page.text');
+    $text = $config->get('text');
     $format = isset($text['format']) ? $text['format'] : filter_default_format();
     $output['#markup'] = check_markup($text['value'], $format);
 
